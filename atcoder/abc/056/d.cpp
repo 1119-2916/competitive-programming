5b4307ec86130efe4c0b6d54f13286084ee55b2d
1119-2916 <ti11192916@gmail.com>
2017-12-16 20:49:30 +0900

    2017-12-16

---
+++ atcoder/abc/056/d.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF 1001000100010001000
+#define MOD 1000000007
+#define EPS 1e-10
+#define int long long
+#define rep(i, N) for (int i = 0; i < N; i++)
+#define Rep(i, N) for (int i = 1; i < N; i++)
+#define For(i, a, b) for (int i = (a); i < (b); i++)
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define i_i pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define vp vector< i_i >
+#define all(a) (a).begin(), (a).end()
+#define Int(x) int x; cin >> x;
+#define int2(x, y) Int(x); Int(y);
+#define int3(x, y, z) Int(x); int2(y, z);
+#define fir first
+#define sec second
+#define ffir first.first
+#define fsec first.second
+#define sfir second.first
+#define ssec second.second
+#define Decimal fixed << setprecision(10)
+
+//int dxy[5] = {0, 1, 0, -1, 0};
+// cmd
+
+signed main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int2(n, k);
+    vector<int> data(n);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
+        sum += data[i];
+    }
+    if (sum < k) {
+        cout << n << endl;
+        return 0;
+    }
+
+    sort(all(data));
+    reverse(all(data));
+
+        vb dp(k, false);
+        dp[0] = true;
+        rep(i, n-1) {
+            for (int j = k - data[i]-1; j >= 0; j--) {
+                if (dp[j]) {
+                    dp[j+data[i]] = true;
+                }
+            }
+        }
+        bool ok = false;
+        for (int i = k-data[n]; i < k; i++) {
+            ok |= data[n];
+        }
+        if (ok) {
+            cout << 0 << endl;
+            return 0;
+        }
+
+    int l = 0, r = n;
+    while (r - l > 1) {
+        int mid = (r + l) / 2;
+        vb dp(k, false);
+        dp[0] = true;
+        rep(i, n) {
+            if (i == mid) continue;
+            for (int j = k - data[i]-1; j >= 0; j--) {
+                if (dp[j]) {
+                    dp[j+data[i]] = true;
+                }
+            }
+        }
+        bool ok = false;
+        for (int i = k-data[mid]; i < k; i++) {
+            ok |= data[mid];
+        }
+        if (ok) {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+
+    cout << n - l +1 << endl;
+
+    return 0;
+}
+
+
+
+
