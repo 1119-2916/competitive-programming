7ef41d2d10ed500a34b3e64f160897e37d635943
T.Ikeda <ti11192916@gmail.com>
2018-09-27 09:05:50 +0900

    2018-09-27

--- atcoder/abc/110/d.cpp
+++ atcoder/abc/110/d.cpp
@@ -34,11 +34,126 @@ using namespace std;
 //int dxy[5] = {0, 1, 0, -1, 0};
 // BIT DiscreteFourierTransform FFT avl cmd cusum dijkstra dinic geo2 gin graph kruskal lca lcm matrix ncm next_combination ppuf segtree st tmp topcoder uf vi 
 
+int fact[1145140];
+int pascal_table[100][100];
+
+/*
+ * tableをdoubleにしてncmの割合も求められる
+ * 1/2して下ってゆくように書き換えてその場で作る.
+ */
+void make_pascal(int mod)
+{
+    rep(i, 100) {
+        pascal_table[i][0] = 1;
+        for (int j = 1; j < i; j++) {
+            pascal_table[i][j] = (pascal_table[i-1][j-1] +
+                    pascal_table[i-1][j]) % mod;
+        }
+        pascal_table[i][i] = 1;
+    }
+}
+
+int gcd(int a, int b) { if (b == 0) return a; return gcd(b, a%b); }
+
+int extgcd(int a, int b, int &x, int &y)
+{
+    int d = a;
+    if (b != 0) {
+        d = extgcd(b, a % b, y, x);
+        y -= (a / b) * x;
+    } else { x = 1; y = 0; }
+    return d;
+}
+
+int repow(int x, int n, int mod)
+{
+    if (n == 0) return 1;
+    int res = repow(x * x % mod, n / 2, mod);
+    if (n & 1) res = res * x % mod;
+    return res;
+}
+
+int mod_inverse(int a, int m)
+{
+    int x, y;
+    extgcd(a, m, x, y);
+    return ( m + x % m ) % m;
+}
+
+int table(int n)
+{
+    int sum = 1;
+    fact[0] = 1;
+    Rep(i, n+2) {
+        sum *= i;
+        sum %= MOD;
+        fact[i] = sum;
+    }
+}
+
+int mod_fact(int n, int p, int &e)
+{
+    e = 0;
+    if (n == 0) return 1;
+    int res = mod_fact(n / p, p, e);
+    e += n / p;
+
+    if (n / p % 2 != 0) return res * (p - fact[n % p]) % p;
+    return res * fact[n % p] % p;
+}
+
+/*
+ * nCr mod p
+ * nHr = (n+r-1)Cr
+ * 必ずtable()を呼んでから使う.
+ * n = 1001000くらいで
+ */
+int mod_comb(int n, int k, int p) {
+    if (n < 0 || k < 0 || n < k) return 0;
+    int e1, e2, e3;
+    int a1 = mod_fact(n, p, e1), a2 = mod_fact(k, p, e2), a3 = mod_fact(n-k, p, e3);
+    if (e1 > e2 + e3) return 0;
+    return a1 * mod_inverse(a2 * a3 % p, p) % p;
+}
+
+int ncm(int n, int r)
+{
+    if (r == 0) return 1;
+    return (n-r+1) * ncm(n, r-1) / r;
+}
+
+bool is_prime(int n)
+{
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 signed main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
+    
+    table(1001000);
+    int2(n, m);
 
+    vi yui;
+    for (int i = 2; !is_prime(m); i++) {
+        int cnt = 0;
+        while (m % i == 0) {
+            m /= i;
+            cnt++;
+        }
+        yui.pb(cnt);
+    }
+    if (m != 1) yui.pb(1);
+
+    int ans = 1;
+    rep(i, yui.size()) {
+        ans = (ans * mod_comb(n + yui[i] - 1, yui[i], MOD)) % MOD;
+    }
+    cout << ans << endl;
 
 
     return 0;
---
+++ csacademy/070/hoge.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF 1001000100010001000
+#define MOD 1000000007
+#define EPS 1e-10
+#define int long long
+#define rep(i, N) for (int i = 0; i < N; i++)
+#define Rep(i, N) for (int i = 1; i < N; i++)
+#define For(i, a, b) for (int i = (a); i < (b); i++)
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define vp vector< pii >
+#define all(a) (a).begin(), (a).end()
+#define Int(x) int x; cin >> x;
+#define int2(x, y) Int(x); Int(y);
+#define int3(x, y, z) Int(x); int2(y, z);
+#define in(x, a, b) ((a) <= (x) && (x) < (b))
+#define fir first
+#define sec second
+#define ffir first.first
+#define fsec first.second
+#define sfir second.first
+#define ssec second.second
+#define Decimal fixed << setprecision(10)
+
+//int dxy[5] = {0, 1, 0, -1, 0};
+// cmd
+
+/**
+ * 動的 CHT 単調性無しok 各操作平均 O(logN) 
+ * 木は偏る（真理）
+ * **必ず厳密な探索範囲 (minn, maxx) を与えること**
+ * T = int 以外で未検証
+ */
+template <typename T, const T id = numeric_limits<T>::min()>
+class CHT {
+    using L = T;   // 返り値の型 (縦軸)
+    struct Line {
+        T a, b;
+        Line (T a = 0, T b = 0) : a(a), b(b) {}
+        L get (T x) { return a * x + b; }
+    };
+    struct Node {
+        Line line;
+        Node *lch, *rch;
+        Node (Line line) : line(line), lch(nullptr), rch(nullptr) {}
+    };
+
+    private :
+    // 探索区間（横軸）の最小、最大
+    const T minn, maxx; 
+    Node *root;
+
+    // [lb, ub]
+    Node* update(Node *p, T lb, T ub, Line& line) {
+        if (!p) { return new Node(line); }
+        if (p->line.get(lb) >= line.get(lb) && 
+                p->line.get(ub) >= line.get(ub)) { return p; }
+        if (p->line.get(lb) <= line.get(lb) && 
+                p->line.get(ub) <= line.get(ub)) {
+            p->line = line;
+            return p;
+        }
+        T mid = (lb + ub) / 2;
+        if (p->line.get(mid) < line.get(mid)) { swap(p->line, line); }
+        if (p->line.get(lb) <= line.get(lb)) {
+            p->lch = update(p->lch, lb, mid, line);
+        } else {
+            p->rch = update(p->rch, mid, ub, line);
+        }
+        return p;
+    }
+    L query(Node *p, T lb, T ub, T t) {
+        if (!p) { return id; }
+        T mid = (lb + ub) / 2;
+        if (t <= mid) {
+            return max(p->line.get(t), query(p->lch, lb, mid, t));
+        }
+        return max(p->line.get(t), query(p->rch, mid, ub, t));
+    }
+
+    public :
+    CHT (const T minn, const T maxx) : minn(minn), maxx(maxx), root(nullptr) {}
+    void insert(T a, T b) {
+        Line line(a, b);
+        root = update(root, minn, maxx, line);
+    }
+    L get(T x) {
+        return query(root, minn, maxx, x);
+    }
+
+};
+
+signed main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int2(n, k);
+    vector<int> data(n);
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
+    }
+
+    vvi dp(k+1, vi(n+1, INF));
+    dp[0][0] = 0;
+    rep(i, k) {
+        CHT<int> funami(0, 101000);
+        rep(a, n) {
+            funami.insert(2 * data[a], -(dp[i][a] + data[a] * data[a]));
+            dp[i+1][a+1] = data[a] * data[a] - funami.get(data[a]);
+        }
+    }
+    cout << dp[k][n] << endl;
+
+    return 0;
+}
