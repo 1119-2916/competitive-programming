b1d8dfe5286e32312dce1b01c91e196940c02d59
T.Ikeda <ti11192916@gmail.com>
2018-10-23 06:57:23 +0900

    2018-10-23

--- atcoder/agc/016/c.cpp
+++ atcoder/agc/016/c.cpp
@@ -35,6 +35,39 @@ using namespace std;
 signed main()
 {
 
+    int2(H, W);
+    int2(h, w);
+
+    vvi funami(H, vi(W, 1));
+    Rep(i, H+1) {
+        Rep(j, W+1) {
+            if (i % h == 0 && j % w == 0) {
+                funami[i-1][j-1] = -(h*w+1);
+            } else if (i % h == 1 && j % w == 1) {
+                funami[i-1][j-1]++;
+            }
+        }
+    }
+
+    int sum = 0;
+    rep(i, funami.size()) {
+        rep(j, funami[i].size()) {
+            sum += funami[i][j];
+        }
+    }
+
+    if (sum <= 0) {
+        cout << "No" << endl;
+    } else {
+        cout << "Yes" << endl;
+        rep(i, funami.size()) {
+            rep(j, funami[i].size()) {
+                std::cout << funami[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
     return 0;
 }
 
